76d62fc0f4f186d6943b21c59d917014445bb143
Piotr Kiljanek <kiljanek2005@gmail.com>
2018-12-06 13:02:53 +0100

    Update sendMail.sh

--- Bash/sendMail.sh
+++ Bash/sendMail.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 echo "Skrypt wykonał się pomyślnie, wszystko jest ok" | mailx -s "Test zadania" "piotr@gmail.pl"
+
+#lub za pomocą mutt-a
+
+mutt -s "test jak nic " piotrk@suzuki.com.pl < /dev/null
