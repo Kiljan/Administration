85b390a0a1d672c829013640f659a53a1f8621dd
Piotr Kiljanek <kiljanek2005@gmail.com>
2020-02-05 16:30:38 +0100

    Update sendMail.sh

--- BASH/MAIL/sendMail.sh
+++ BASH/MAIL/sendMail.sh
@@ -1,9 +1,19 @@
-#!/bin/bash
-
 # I can send by sendmail
 echo "Some message" | mailx -s "Header" "mail0@domain.com"
 
 # or using mutt
+# i can configure client to send stuff
+# file to write settings is .muttrc
+# Content of .muttrc
+# set imap_user = "mail@adress.com"
+# set imap_pass = "password"
+# set smpt_url = "smtp:url:port"
+# set smtp_pass = "password"
+# set from = "mail@adress.com"
+# set realname "name to show"
+# set folder = "imaps:url:port"
+# set ssl_starttls = yes
+# set ssl_force_tls = yes
 
 MAIL_LIST="mail1@domain.com, mail2@domain.com , mail3@domain.com"
 FILE_TO_SEND="/var/spool/cron/posts"
