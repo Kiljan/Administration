1100773a82ed702dc9248e44b259342a4466a962
Piotr Kiljanek <kiljanek2005@gmail.com>
2020-08-23 17:37:23 +0200

    Update runJarOnMeany.sh

--- BASH/OTHER/runJarOnMeany.sh
+++ BASH/OTHER/runJarOnMeany.sh
@@ -4,5 +4,5 @@ listVar=("FILEONE.TXT" "FILETWO.TXT" "FILEN.TXT")
 
 for i in "${listVar[@]}"; do
     echo "Start for $i"
-    java -jar /home/lol/tasks/mainJar.jar /mnt/OUT/$i
+    java -jar @HOME/tasks/mainJar.jar /mnt/OUT/$i
 done
